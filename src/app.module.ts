395b38e9191bcd92fa01b030bd3f093c0216db08
Duc Doanh <duc.doanh@urbn8.com>
2024-09-10 18:11:49 +0700

    feat: init vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { InfrastructureModule } from "./infrastructure/infrastructure.module";
 import { UserModule } from "./domains/User/User.module";
+import { AppController } from "./app.controller";
 
 @Module({
+  controllers: [AppController],
   imports: [UserModule, InfrastructureModule],
 })
 export class AppModule {}
