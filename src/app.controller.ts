8d7d0ead33583306bb9426960be6ed4f190ae381
Duc Doanh <duc.doanh@urbn8.com>
2025-01-25 00:17:34 +0700

    fix: typo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,12 @@ import { ConfigService } from "@nestjs/config";
 
 @Controller()
 export class AppController {
-  constructor(private readonly congfigService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
   getHello() {
     return {
-      message: `Hello this is a non-profit project to support students to learn English:\n env ${process.env.JWT_ACCESSTOKEN_SECRET};\n env config service: ${this.congfigService.get("jwt.jwtAccessSecret")}`,
+      message: `Hello this is a non-profit project to support students to learn English:\n env ${process.env.JWT_ACCESSTOKEN_SECRET};\n env config service: ${this.configService.get("jwt.jwtAccessSecret")}`,
     };
   }
 }
