21a3dcda5990a0842bebab65f917c798fe90e574
Duc Doanh <duc.doanh@urbn8.com>
2025-02-01 17:01:30 +0700

    feat: handle staging migration

--- knexfile.ts
+++ knexfile.ts
@@ -29,22 +29,6 @@ const config: { [key: string]: Knex.Config } = {
       max: appConfigs.maxPool,
     },
   },
-
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password",
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations",
-  //   },
-  // },
 };
 
 module.exports = config;
